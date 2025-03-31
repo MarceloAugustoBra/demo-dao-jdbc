ac0d33aeae1e3e4c5209b9c6fdf773b8d6e7d826
Marcelo Augusto <marceloaugustobra@gmail.com>
2025-03-31 12:35:27 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,12 +1,20 @@
 package Application;
 
+import java.util.Date;
+
 import Model.entities.Department;
+import Model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		Department obj =  new Department(1, "books");
 		System.out.println(obj);
+		
+		Seller seller =  new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		System.out.println(seller);
 	}
+	
+	
 
 }
