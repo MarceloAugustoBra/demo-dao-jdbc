bf754646d99ecac3ef6f464fb16372d66a1d15bd
Marcelo Augusto <marceloaugustobra@gmail.com>
2025-04-01 12:33:36 -0300

    Update implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -32,12 +32,18 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: Insert:");
+		System.out.println("\n=== TEST 4: Insert:");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(),
 				4000.00, department );
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted!New ID = " + newSeller.getId());
 		
+		
+		System.out.println("\n=== TEST 5: Seller Update");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		sellerDao.update(seller);
+		System.out.println("UPDATE COMPLETED");
 	}
 	
 	
