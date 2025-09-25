bc71d08e4b8dc82a138b24910668a5160637dd66
Marcelo Augusto <marceloaugustobra@gmail.com>
2025-09-25 16:11:31 -0300

    update implementation

--- src/application/program.java
+++ src/application/program.java
@@ -44,6 +44,14 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted: new ID = " + newSeller.getId());
 		
+		System.out.println();
+
+		System.out.println("=== Teste 5: seller update");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update complete!");
 		
 		
 		
